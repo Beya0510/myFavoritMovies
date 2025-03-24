08d743de3597f7f54c29a43bb10227a219cbff5d
Jean-Marie BEYAMULUMBA <beyamulumbajm@gmail.com>
2025-03-24 19:37:29 +0100

    affichage page recherche

---
+++ Favies/Services/GetMoviesService.cs
@@ -0,0 +1,27 @@
+namespace Favies.Services
+{
+    using System.Net.Http;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+
+
+
+    public class GetMoviesService
+    {
+        private readonly HttpClient _httpClient;
+
+        public GetMoviesService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        
+        string url = "https://www.omdbapi.com/";
+        public async Task<T?> GetMoviesAsync<T>(string url) where T : class
+        {
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(responseContent);
+        }
+    }
+}  
